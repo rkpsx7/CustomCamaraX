69ca8c7e2e81fc5db4599a678664f58d46e364eb
AkashKumarPrajapat <rkpsx7@gmail.com>
2023-03-16 11:56:43 +0530

    Refactored and cleaned the code

--- app/src/main/java/dev/akash/customcamarax/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/dev/akash/customcamarax/viewmodel/ViewModelFactory.kt
@@ -3,7 +3,9 @@ package dev.akash.customcamarax.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class ViewModelFactory @Inject constructor(
     private val viewModelMap: Map<Class<*>, @JvmSuppressWildcards ViewModel>
 ) : ViewModelProvider.Factory {
